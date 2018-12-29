019367ad00d3cefba9734d9a74992d4db0f7d4e8
ajajitesh <kumar2024aj@gmail.com>
2018-12-29 07:08:37 +0000

    Initial commit

---
+++ src/main/java/sleeping/idiot/ajitesh/App.java
@@ -0,0 +1,13 @@
+package sleeping.idiot.ajitesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sleeping/idiot/ajitesh/AppTest.java
@@ -0,0 +1,20 @@
+package sleeping.idiot.ajitesh;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
